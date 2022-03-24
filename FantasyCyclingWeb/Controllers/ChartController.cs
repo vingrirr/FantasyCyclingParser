6489810a6cf94126af9cf8c4ef36f8d521194b03
Ryan Vingris <ryan.vingris@gmail.com>
2022-03-24 15:26:25 -0400

    initial bar chart race

---
+++ FantasyCyclingWeb/Controllers/ChartController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FantasyCyclingParser;
+using FantasyCyclingWeb.Models;
+using Common;
+
+
+namespace FantasyCyclingWeb.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        public ActionResult Index()
+        {
+
+            FantasyYearConfig config = Repository.FantasyYearConfigGetDefault();
+            PDC_Season season = Repository.PDCSeasonGet(config.Year);
+
+            RaceSeasonViewModel vm = new RaceSeasonViewModel(season, config);
+            
+
+            return View();
+        }
+    }
+}
