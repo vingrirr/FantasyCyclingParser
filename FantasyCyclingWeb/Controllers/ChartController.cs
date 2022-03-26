d5488ae79405116756c3cc30f7835c0ca1636dd0
Ryan Vingris <ryan.vingris@gmail.com>
2022-03-26 14:13:52 -0400

    hjhgh

--- FantasyCyclingWeb/Controllers/ChartController.cs
+++ FantasyCyclingWeb/Controllers/ChartController.cs
@@ -19,10 +19,32 @@ namespace FantasyCyclingWeb.Controllers
             FantasyYearConfig config = Repository.FantasyYearConfigGetDefault();
             PDC_Season season = Repository.PDCSeasonGet(config.Year);
 
-            RaceSeasonViewModel vm = new RaceSeasonViewModel(season, config);
-            
+            RaceSeasonViewModel rs = new RaceSeasonViewModel(season, config);
 
-            return View();
+            List<string> raceList = new List<string>();
+
+            BarChartRaceViewModel vm = new BarChartRaceViewModel(); 
+            foreach (FantasyResult fr in rs.FantasyResults)
+            {
+                vm.RaceList.Add(fr.Race.EventName);
+                BarChartRaceItem item = new BarChartRaceItem();
+                item.Name = fr.Race.EventName;
+
+                foreach (PDCTeamPoints tp in fr.Points)
+                {
+                    //BarChartDataPoint d = new BarChartDataPoint();
+                    KeyValuePair<string, int> kvp = new System.Collections.Generic.KeyValuePair<string, int>(tp.Name, tp.RunningTotalPoints);
+                    
+                    //d.Key = tp.Name;
+                    //d.Value = tp.RunningTotalPoints;
+                    item.Items.Add(kvp);
+                }
+
+                vm.BarChartData.Add(item);
+                
+            }
+
+            return View(vm);
         }
     }
 }
---
+++ FantasyCyclingWeb/Models/BarChartRaceViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FantasyCyclingWeb.Models
+{
+    public class BarChartRaceViewModel
+    {
+        public BarChartRaceViewModel()
+        {
+            RaceList = new List<string>();
+            BarChartData = new List<BarChartRaceItem>();
+        }
+
+        public List<string> RaceList { get; set; }
+        public List<BarChartRaceItem> BarChartData { get; set; }
+    }
+
+    public class BarChartRaceItem
+    {
+        public BarChartRaceItem()
+        {
+            Items = new List<KeyValuePair<string, int>>();
+        }
+
+        public string Name { get; set; }
+        public List<KeyValuePair<string, int>> Items { get; set; }
+        
+    }
+
+    public class BarChartDataPoint
+    {
+        public BarChartDataPoint()
+        {
+
+        }
+
+        public string Key { get; set; }
+        public int Value { get; set; }
+    }
+}
