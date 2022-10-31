d763d2e5dadadbbf561e414fb3e4a10e1d7de94c
Ryan Vingris <ryan.vingris@gmail.com>
2022-10-31 13:10:55 -0400

    mongo instructions

---
+++ FantasyDraftBlazor/Data/RiderService.cs
@@ -0,0 +1,14 @@
+﻿using FantasyCyclingParser;
+
+namespace FantasyDraftBlazor.Data
+{
+    public class RiderService
+    {
+        public Task<RiderPhoto> GetPhotoAsync()
+        {
+            RiderPhoto photo = Repository.RiderPhotoGetAll().First();
+            return Task.FromResult(photo); 
+        }
+
+    }
+}
