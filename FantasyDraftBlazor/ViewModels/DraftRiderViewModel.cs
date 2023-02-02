9918da2fcc6903ee8b00d6853e7f1a9275c8845d
Ryan Vingris <ryan.vingris@gmail.com>
2023-02-02 12:50:19 -0500

    before migrating to use draftteamviewmodel

---
+++ FantasyDraftBlazor/ViewModels/DraftRiderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using FantasyCyclingParser;
+
+namespace FantasyDraftBlazor.ViewModels
+{
+    public class DraftRiderViewModel
+    {
+        public DraftRiderViewModel()
+        {
+
+        }
+
+        public Rider Rider { get; set; }
+        public bool IsDrafted { get; set; }
+    }
+}
---
+++ FantasyDraftBlazor/ViewModels/DraftTeamViewModel.cs
@@ -0,0 +1,22 @@
+﻿using FantasyCyclingParser;
+
+namespace FantasyDraftBlazor.ViewModels
+{
+    public class DraftTeamViewModel
+    {
+        public DraftTeamViewModel()
+        {
+            Riders = new List<DraftRiderViewModel>();
+        }
+        public string ID { get; set; }
+
+        public bool IsDraftPDCTeam { get; set; }
+
+        public bool Is35Team { get; set; }
+
+        public string PDCTeamName { get; set; }
+        public PDCTeam Model { get; set; }
+        public List<DraftRiderViewModel> Riders { get; set; }
+        
+    }
+}
